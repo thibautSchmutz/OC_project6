b12a348e5cb9a462f377dd75044a3a5afd6a2276
thibaut schmutz <thibaut.schmu@gmail.com>
2020-08-25 18:29:31 +0200

    :sparkles: (prod/dev script) each node mode has its own script that connects to the databse with different access.

--- config/db.js
+++ config/db.js
@@ -1,14 +1,27 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+// Lecture des variables d'environnement
+dotenv.config({ path: "./config/srt.env" });
+
+let mongoUri = process.env.PROD_MODE
+  ? process.env.MONGO_URI_PROD
+  : process.env.MONGO_URI_DEV;
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
+  const conn = await mongoose.connect(mongoUri, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
   });
 
-  console.log(`MongoDB connecté: ${conn.connection.host}`.bgGreen.black.bold);
+  const nodeMode = process.env.PROD_MODE ? "production" : "developpement";
+
+  console.log(
+    `MongoDB connecté: ${conn.connection.host} en mode ${nodeMode}`.bgGreen
+      .black.bold
+  );
 };
 
 module.exports = connectDB;
